e6a7c19e9471aef00346a93918ae40a8305a3a54
Leandro Souza <leandror.dev@gmail.com>
2020-04-08 21:23:46 -0300

    Update and validade update.

--- backend/src/index.js
+++ backend/src/index.js
@@ -7,10 +7,9 @@ app.use(express.json());
 const projects = [];
 
 app.get('/projects', (req, res) => {
-   
    // Verificando a existência de projetos. Caso não exista retorna um erro.
    if (projects[0] === undefined) {
-      return res.status(404).json({eror: 'Projects do not exist'})
+      return res.status(404).json({eror: 'Projects not found'})
    }
 
    // Retornando Projetos
@@ -28,7 +27,22 @@ app.post('/projects', (req, res) => {
 });
 
 app.put('/projects/:id', (req, res) => {
-   return res.json(['Projeto 5', 'Projeto 2', 'Project 3', 'Project 4'])
+   const {id} = req.params;
+
+   const projectIndex = projects.findIndex(project => project.id = id);
+   const {title = projects[projectIndex].title, owner = projects[projectIndex].owner} = req.body;
+
+   if (projectIndex < 0) {
+      return res.status(404).json({eror: `Project with id ${id} not found`})
+   }
+
+   const project = {
+      title, 
+      owner
+   }
+   projects[projectIndex] = project
+
+   return res.json(project)
 });
 
 app.delete('/projects/:id', (req, res) => {
