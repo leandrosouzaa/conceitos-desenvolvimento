9a1dd2f81434f165f6f459dcf0330c9f0279c67a
Leandro Souza <leandror.dev@gmail.com>
2020-04-09 17:16:51 -0300

    Estados da aplicação

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,21 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 import Header from './components/Header'
 
 export default function App() {
+   const [projects, setProjects] = useState([])
+
+   function handleAddProject() {
+      setProjects([...projects, `Novo Projeto ${Date.now()}`]);
+      console.log(projects)
+   }
    return (
-      <Header />
+      <>
+         <Header title="Projects"/>
+         <ul>
+            {projects.map(project => <li key={project}>{project}</li>)}
+         </ul>
+         <button type="button" onClick={handleAddProject}>Adicionar Projeto</button>
+      </>
    )
 }
