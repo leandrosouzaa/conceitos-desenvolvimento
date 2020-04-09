5bdc6ce913ca522e6c96804ca716e71e50451174
Leandro Souza <leandror.dev@gmail.com>
2020-04-09 18:31:18 -0300

    Configuração do Cors

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,8 +1,12 @@
-const express = require('express')
-const {uuid, isUuid} = require('uuidv4')
+const express = require('express');
+const {uuid, isUuid} = require('uuidv4');
+const cors = require('cors')
 
 const app = express();
+
+app.use(cors());
 app.use(express.json());
+
 
 const projects = [];
 
