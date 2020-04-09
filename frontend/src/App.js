3291cbadbb4016b8a06614126036997b8c196839
Leandro Souza <leandror.dev@gmail.com>
2020-04-09 18:23:54 -0300

    Configuração para o uso de imagens

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,24 @@
 import React, {useState} from 'react';
 
-import Header from './components/Header'
-
 import './App.css'
 
+import Header from './components/Header';
+import backgroundImage from './assets/background.png';
+
+
 export default function App() {
-   const [projects, setProjects] = useState([])
+   const [projects, setProjects] = useState([]);
 
    function handleAddProject() {
       setProjects([...projects, `Novo Projeto ${Date.now()}`]);
-      console.log(projects)
+      console.log(projects);
    }
    return (
       <>
          <Header title="Projects"/>
+         
+         <img width={350} src={backgroundImage} alt="projects"/>
+
          <ul>
             {projects.map(project => <li key={project}>{project}</li>)}
          </ul>
