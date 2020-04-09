b65b924c916d87d588885e4f63948157501d7c79
Leandro Souza <leandror.dev@gmail.com>
2020-04-09 17:23:39 -0300

    Adicionando estilos na aplicação

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,8 @@ import React, {useState} from 'react';
 
 import Header from './components/Header'
 
+import './App.css'
+
 export default function App() {
    const [projects, setProjects] = useState([])
 
